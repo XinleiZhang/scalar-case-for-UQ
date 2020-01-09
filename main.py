fdfcb3a0942a40d48e683b3a84dde8a16cf938b4
xinleizhang <xinleizhang@vt.edu>
2020-01-09 14:09:16 +0100

    clean

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-
-# -*- coding: utf-8 -*-
-"""
-Created on Sep. 07 2018
-simple forword model
-"""
-
+#!/usr/bin/env python3
 
 import numpy as np
 import analysis
@@ -12,28 +6,30 @@ import numpy.linalg as la
 import pdb
 import os
 
+
 def forwardmodel(X, q):
     y = 1. + np.sin(np.pi * X) + q
     return y
-#M:forwardmodel E:10^ iter:maxiteration; 
-casename = 'M1_E4_EnRML_iter30_NRobs'
-ensemblemethod= 'EnRML'
-#ensemble parameter
-r = 0.5
 
-#determined parameters
-sigmap = 0.1
-sigmaq = 0.03
-sigmad = 0.1
+# user-defined variables
 
-Nen = 10**4                  #Ensemble size
-maxiter = 30                #maximum iteration number
-cri = 1.e-10
+casename = 'E4_EnRML_iter30'
+ensemblemethod= 'EnRML' #'EnKF_MDA' 'EnKF'
+
+r = 0.5 # step parameter for EnRML
+
+sigmap = 0.1    # std of prior x
+sigmaq = 0.03   # std of model error
+sigmad = 0.1    # std of observation error
+
+Nen = 10**4     # ensemble size
+maxiter = 30    # maximum iteration number
+cri = 1.e-10    # convergence criteria
+
 
 Cxx = sigmap*sigmap
 Cqq = sigmaq*sigmaq
 Cdd = sigmad*sigmad
-
 
 x0 = np.random.normal(0, sigmap, Nen)
 q = np.random.normal(0, sigmaq, Nen)
@@ -42,6 +38,10 @@ pertObs = np.random.normal(0., sigmad, Nen)
 x = x0
 beta = np.zeros(Nen)
 converge_flag = 'False'
+
+if not os.path.exists('./postprocessing/'+casename):
+    os.mkdir('./postprocessing/'+casename)
+
 for iter in range(maxiter):
     y = forwardmodel(x,q)
     
@@ -55,7 +55,7 @@ for iter in range(maxiter):
     
     elif ensemblemethod== 'EnKF_MDA':
         alpha = maxiter
-        pertObs = np.random.normal(0., sigmad, Nen)# note: need perturb Obs for each iter 
+        pertObs = np.random.normal(0., sigmad, Nen)
         obs =1.+ np.sqrt(alpha)* pertObs
         analysismatrix = analysis.EnKF_MDA(x, y, obs,Cdd,Nen,alpha)
         dx = analysismatrix*(obs-y)
@@ -63,37 +63,18 @@ for iter in range(maxiter):
         if abs(np.mean(dx)) < cri: converge_flag = 'True'
 
     elif ensemblemethod=='EnRML':
-        #pertObs = np.random.normal(0., sigmad, Nen)
-        obs = 1. + pertObs # note: fix pertObs can lead to better result
+        obs = 1. + pertObs
         GNGainMatrix, penalty = analysis.EnRML(x0, y0, x, y, obs,Cdd,Nen)
         dx = penalty - GNGainMatrix * (y - obs)
         x = r*x0 + (1-r)*x + r * dx
         if abs(np.mean(dx)) < cri: converge_flag = 'True'
 
-    elif ensemblemethod=='EnVar':
-        epsilon = 0.01
-        U = np.eye(Nen)
-        pertObs = np.random.normal(0., sigmad, Nen)
-        obs = 1. + pertObs
-        A0 = x0-np.mean(x0)
-        dx = A0.dot(beta)
-        xmean = np.mean(x0) + dx
-        x = xmean + epsilon*(x0 - np.mean(x0))
-        beta, Hes = analysis.EnVar(beta, x, y, obs, Cdd, Nen, epsilon)
-        if iter == (maxiter -1):
-            eigenv, eigenvec= la.eig(Hes)
-            eigenvnew = np.diag(1./(np.sqrt(eigenv)))
-            Hes_invroot= np.dot(eigenvec.dot(eigenvnew), la.inv(eigenvec)) 
-            x = abs(xmean + np.sqrt(Nen-1)* np.dot(A0.dot( Hes_invroot),U))
-            y = forwardmodel(x, q)
     if converge_flag=='True':
         #pdb.set_trace()
         print ('reach convergence condition at iteration', iter )
         break
     if iter == (maxiter-1): print ('reach max iteration')
 
-    if not os.path.exists('./postprocessing/'+casename):
-        os.mkdir('./postprocessing/'+casename)
 
 np.savetxt('./postprocessing/'+casename+'/posterior_x.txt',x)
 np.savetxt('./postprocessing/'+casename+'/posterior_y.txt',y)
--- pdfplot.py
+++ pdfplot.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep  6 18:11:11 2018
-https://python-graph-gallery.com/85-density-plot-with-matplotlib/
-@author: zxl
-"""
 
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import gaussian_kde
-#import seaborn as sns
+import seaborn as sns
 import matplotlib as mpl
 
 # set plot properties
-
 params = {
     'text.latex.preamble': ['\\usepackage{gensymb}'],
     'image.origin': 'lower',
@@ -33,15 +26,14 @@ params = {
 
 mpl.rcParams.update(params)
 
-ensemblemethod = 'EnRML' #'EnKF_MDA' 'EnKF'
-casefolder = 'E2_'+ensemblemethod+'_iter30'
-res = 'posterior' #'prior'# 'posterior'
+# define which case to plot
+ensemblemethod = 'EnRML' #'EnRML' EnKF_MDA' 'EnKF'
+res = 'posterior' #'prior'or 'posterior'
+casefolder = 'E4_'+ensemblemethod+'_iter30'
 
 # load file
 x= np.loadtxt('./postprocessing/' + casefolder +'/' +res+'_x.txt')
 y = np.loadtxt('./postprocessing/' + casefolder +'/' +res+'_y.txt')
-
-plt.plot(x,y)
 
 fig, ax = plt.subplots()
 
@@ -52,8 +44,8 @@ ax.set_ylim([0,2])
 
 ax.spines['left'].set_linewidth(2)
 ax.spines['bottom'].set_linewidth(2)
-ax.spines['right'].set_linewidth(2) #set_visible(False)
-ax.spines['top'].set_linewidth(2) #set_visible(False)
+ax.spines['right'].set_linewidth(2)
+ax.spines['top'].set_linewidth(2)
 
 colors = [(1,1,1)] + [(plt.cm.jet(i)) for i in range(1,256)]
 mymap = mpl.colors.LinearSegmentedColormap.from_list('mymap', colors, N=256)
@@ -62,13 +54,8 @@ data = np.array([x, y])
 
 kde = gaussian_kde(data)
 nbins = 100
-xx, yy = np.mgrid[x.min():x.max():nbins*1j, y.min():y.max():nbins*1j] #-0.4:0.4:0.001, 0:2:0.002]
+xx, yy = np.mgrid[x.min():x.max():nbins*1j, y.min():y.max():nbins*1j]
 density = kde(np.c_[xx.flat, yy.flat].T).reshape(xx.shape)
- 
-sns.kdeplot(x,y,n_levels=500,cmap=mymap, shade=True,shade_lowest=True,cbar=True)
-ax = sns.kdeplot(x,y,n_levels=30,cmap=mymap, shade=True,shade_lowest=False,
-            cbar=True)
-cset = ax.contourf(xx, yy, density, 500, cmap=mymap, vmin=0,vmax=80)
 
 cset = ax.pcolormesh(xx, yy, density, cmap=mymap, vmin=0, vmax=160,
                  shading='gouraud')
